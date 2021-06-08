2f69ce3c7b34bb0eddda1722dd86e8f63ca28c5d
tdyleuth <tony.dyleuth@gmail.com>
2021-06-07 18:52:12 -0700

    Update API/Server

--- build/index.js
+++ build/index.js
@@ -9,12 +9,16 @@ const logger = require('morgan');
 const connectDB = require('./config/db.ts');
 const photoRoutes = require('./routes/photoRoutes');
 const app = express_1.default();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Welcome to the Photo Album API!');
+});
 /* Middleware */
 app.use(cors());
 app.use(logger('dev'));
 app.use(express_1.default.json());
-app.use('/api/photos', photoRoutes);
+app.use('/api', photoRoutes);
 app.listen(port, () => {
     console.log(`Server is running on Port ${port}!`);
 });
